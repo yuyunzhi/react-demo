1980f2db3a8d44f92b25ef04b38cff1d7dd8a79c
lambert1129 <lambert1129@foxmail.com>
2020-02-13 14:48:01 +0800

    add demo

--- src/components/clock.js
+++ src/components/clock.js
@@ -5,12 +5,26 @@ class Clock extends React.Component{
         super(props)
         this.state = {date:new Date()}
     }
+    componentDidMount() {
+        console.log('初始化')
 
+        this.timerID=setInterval(()=>{
+            this.tick()
+        },1000)
+    }
+
+    componentWillUnmount() {
+        console.log('结束')
+        clearInterval(this.timerID);
+    }
+    tick(){
+        this.setState({date:new Date()})
+    }
     render(){
         return (
             <div>
                 <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <h2>It is {this.state.date.toLocaleTimeString()}</h2>
             </div>
         )
     }
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
+import Demo from '../components/demo.js'
+import Clock from '../components/clock.js'
+import Toggle from '../components/toggle'
+import LoginControl from '../components/login'
+import Example from '../components/example'
+function App(props) {
+    console.log(props);
 
-function App() {
-  return (
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +24,13 @@ function App() {
           Learn React
         </a>
       </header>
+        <footer>
+            <Example></Example>
+            <LoginControl></LoginControl>
+            <Toggle></Toggle>
+            <Clock></Clock>
+            <Demo age={props.age} name={props.name} family={"123"}></Demo>
+        </footer>
     </div>
   );
 }
