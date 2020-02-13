b182bd741c8501766222ece935dc20dd6d3624f6
lambert1129 <lambert1129@foxmail.com>
2020-02-13 16:20:19 +0800

    添加棋盘demo

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,28 +1,21 @@
 import React from 'react';
-import logo from '../logo.svg';
 import './App.css';
 import Demo from '../components/demo.js'
 import Clock from '../components/clock.js'
 import Toggle from '../components/toggle'
 import LoginControl from '../components/login'
 import Example from '../components/example'
+import ChessBoard from '../components/chess-board'
+
+
+
 function App(props) {
     console.log(props);
 
     return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+          <ChessBoard />
       </header>
         <footer>
             <Example></Example>
