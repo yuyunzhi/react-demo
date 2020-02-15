e007ba26ab394446140760329a5d8fb4f07e96d2
lambert1129 <lambert1129@foxmail.com>
2020-02-15 22:58:37 +0800

    多个useState兼容

--- src/utils/myUserState.js
+++ src/utils/myUserState.js
@@ -2,29 +2,40 @@ import React from 'react';
 import './App.css';
 import * as ReactDOM from "react-dom";
 
-
-let _state
+let _state = []
+let index = 0
 function myUseState (initialValue){
-    _state = _state === undefined ? initialValue :_state
-
+    let currentIndex  = index
+    _state[currentIndex] = _state[currentIndex] === undefined ? initialValue :_state[currentIndex]
     function setState (newState){
-        _state = newState
+        _state[currentIndex] = newState
         render()
     }
-    return [_state,setState]
+    console.log('_state',_state)
+    index++
+    return [_state[currentIndex],setState]
 }
 
-const render = ()=> ReactDOM.render(<App />,document.getElementById('root'))
+const render = ()=> {
+    index = 0 //每次走完重置为0
+    ReactDOM.render(<App />,document.getElementById('root'))
+
+}
 
 
 function App(props) {
     const [n,setN] = myUseState(0)
+    const [m,setM] = myUseState(0)
     console.log('后',n);
     return (
         <div className="App">
             <p>{n}</p>
             <p>
                 <button onClick={()=>setN(n+1)}>+1</button>
+            </p>
+            <p>{m}</p>
+            <p>
+                <button onClick={()=>setM(m+1)}>+1</button>
             </p>
         </div>
     );
