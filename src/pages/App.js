35afb0722cce1acb509957d94ff650375e03825e
lambert1129 <lambert1129@foxmail.com>
2020-02-18 23:22:01 +0800

    useEffect && useLayoutEffect

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,47 +1,53 @@
-import React, {useContext, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-
-const xxx = React.createContext(null)
+/**
+ * 每次render后都会运行useEffect
+ *
+ * 用途：
+ * componentDidMount使用，[]作第二个参数
+ * componentDidUpdate使用，可指定依赖
+ * componentWillUnmount使用,通过return
+ * 以上三种用途同时存在
+ *
+ * 多个useEffect按照顺序依次执行
+ */
 
 function App() {
-    const [theme,setTheme] = useState('red')
+
+    const [n,setN] = useState(0)
+    const onClick = ()=>{
+        setN(i=>i+1)
+    }
+    useEffect(()=>{
+        console.log("只在第一次渲染后执行这句话")
+    },[])
+
+    useEffect(()=>{
+        console.log("任何一个状态变化，第一、二、三……次渲染后执行这句话")
+    })
+
+    useEffect(()=>{
+        console.log("第一次，及n变化了才执行这句话")
+    },[n])
+
+    useEffect(()=>{
+        let timer = setInterval(()=>{
+            console.log("hi")
+
+        },1000)
+        return ()=>{
+            console.log('当页面离开的时候执行这段代码');
+            clearInterval(timer)
+            timer = null
+        }
+    },[])
     return (
-        <xxx.Provider value={{theme,setTheme,a:123}}>
-            <div className={`App ${theme}`}>
-                <p>{theme}</p>
-                <div>
-                    <ChildA></ChildA>
-                </div>
-                <div>
-                    <ChildB></ChildB>
-                </div>
-            </div>
-        </xxx.Provider>
+        <div>
+            n:{n}
+            <button onClick={onClick}>+1</button>
+        </div>
     );
 }
-
-function ChildA() {
-    const arg= useContext(xxx)
-    console.log('arg',arg);
-    const {setTheme} = arg
-    return (
-        <div>
-            <button onClick={() => setTheme("red")}>red</button>
-        </div>
-    )
-}
-
-function ChildB() {
-    const {setTheme} = useContext(xxx)
-    return (
-        <div>
-            <button onClick={() => setTheme("blue")}>blue</button>
-        </div>
-    )
-}
-
-
-
 
 export default App
