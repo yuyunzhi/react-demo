b8c228d76ece7436907fb8504fbe7cdc7e8e1841
yuyunzhi <lambert1129@foxmail.com>
2021-02-17 15:10:27 +0800

    feat: update useRef

--- src/useRef/index.jsx
+++ src/useRef/index.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useRef, useState} from 'react';
-import './App.css';
 
 /**
  * 注意useRef 只有一个current，并且赋值后不会更新App
@@ -10,20 +9,36 @@ import './App.css';
  *  当页面有useState执行了，就会渲染更新useRef的值
  */
 function App(props) {
+
     const nRef = useRef(0)  // {current:0}
     const  [n,setN] = useState(0)
+
     useEffect(()=>{
         console.log('nRef:',nRef);
-    })
-    useEffect(()=>{
         console.log('n:',n);
     })
+
+  useEffect(()=>{
+    let timer = setInterval(()=>{
+      console.log('setInterval[nRef]',nRef)
+    },1000)
+
+    return ()=>{
+      console.log('当页面离开的时候执行这段代码');
+      clearInterval(timer)
+      timer = null
+    }
+  },[nRef.current])
+
     return (
         <div className="App">
+            <p>nRf</p>
             <p>{nRef.current}</p>
             <p>
                 <button onClick={()=>nRef.current=nRef.current + 1}>+1</button>
             </p>
+          <br/>
+          <p>n</p>
             <p>{n}</p>
             <p>
                 <button onClick={()=>setN(n+1)}>+1</button>
